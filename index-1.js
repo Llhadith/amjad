c83e19849638a5fd3bcd200fc0d6143e5cd78adc
وللحديث بقية <116040852+Llhadith@users.noreply.github.com>
2022-10-19 00:50:16 +0300

    Create index-1.js

---
+++ index-1.js
@@ -0,0 +1,45 @@
+    
+    <link href='https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css' rel='stylesheet' type='text/css'/>
+ <script data-cfasync='false' src='https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js'/>
+<script>
+window.cookieconsent.initialise({
+  &quot;palette&quot;: {
+    &quot;popup&quot;: {
+      &quot;background&quot;: &quot;#3937a3&quot;
+    },
+    &quot;button&quot;: {
+      &quot;background&quot;: &quot;#e62576&quot;
+    }
+  },
+  &quot;theme&quot;: &quot;edgeless&quot;,
+  &quot;content&quot;: {
+    &quot;message&quot;: &quot;يستخدم هذا الموقع ملفات تعريف الارتباط لضمان حصولك على أفضل تجربة على موقعنا&quot;,
+    &quot;dismiss&quot;: &quot;موافق&quot;,
+    &quot;link&quot;: &quot;شاهد المزيد&quot;,
+    &quot;href&quot;: &quot;https://al7baqia.blogspot.com/p/blog-page_15.html&quot;
+  }
+});
+</script> 
+  
+  <!-- Google Analytics -->
+<script>/*<![CDATA[*/(function(i,s,o,g,r,a,m){i[&#39;GoogleAnalyticsObject&#39;]=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,&#39;script&#39;,&#39;https:ga(&#39;create&#39;,&#39;UA-XXXXX-Y&#39;,&#39;auto&#39;);ga(&#39;send&#39;,&#39;pageview&#39;);/*]]>*/</script>
+<!-- End Google Analytics -->
+
+    <script>/*<![CDATA[*/function rmurl(e,t){var r=new RegExp(/\?m=0|&m=0|\?m=1|&m=1/g);return r.test(e)&&(e=e.replace(r,""),t&&window.history.replaceState({},document.title,e)),e}const currentUrl=rmurl(location.toString(),!0);/*]]>*/</script>
+
+  
+  
+<script type='text/javascript'>
+//<![CDATA[
+var lazyadsense2 = false;
+window.addEventListener("scroll", function(){
+if ((document.documentElement.scrollTop != 0 && lazyadsense2 === false) || (document.body.scrollTop != 0 && lazyadsense2 === false)) {
+(function() { var ad = document.createElement('script'); ad.setAttribute('data-ad-client','ca-pub-7559639150122331'); ad.async = true; ad.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ad, sc); })();
+lazyadsense2 = true;
+  }
+}, true);
+//]]>
+</script>
+
+  <script async='async' nonce='P_dEc2tHGIfvSNDZKtUEFA' src='https://fundingchoicesmessages.google.com/i/pub-7559639150122331?ers=1'/><script nonce='P_dEc2tHGIfvSNDZKtUEFA'>(function() {function signalGooglefcPresent() {if (!window.frames[&#39;googlefcPresent&#39;]) {if (document.body) {const iframe = document.createElement(&#39;iframe&#39;); iframe.style = &#39;width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;&#39;; iframe.style.display = &#39;none&#39;; iframe.name = &#39;googlefcPresent&#39;; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script>
+
